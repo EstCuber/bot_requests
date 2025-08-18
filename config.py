653afe7f582db5360e857b082193ce13b4a8b19a
Conditor <welcomebrianemusp@gmail.com>
2025-08-18 07:38:46 +0700

    Изменил старт бота и получение данных из env

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
-import os
-from dotenv import load_dotenv
+# import os
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# BOT_TOKEN = os.getenv('BOT_TOKEN')
+# ADMIN_ID = os.getenv('ADMIN_ID')
+# DB_URL = os.getenv('DB_URL')
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-ADMIN_ID = os.getenv('ADMIN_ID')
-DB_URL = os.getenv('DB_URL')
-
+from pydantic_settings import BaseSettings, SettingsConfigDict
 import logging.config
 
-def setup_logging():
+def setup_logging() -> None:
     LOGGING_CONFIG = {
         'version': 1,
         'disable_existing_loggers': False,
@@ -32,4 +33,14 @@ def setup_logging():
             }        },        'root': {
             'level': 'DEBUG',
             'handlers': ['console', 'file']
-        }    }    logging.config.dictConfig(LOGGING_CONFIG)
+        }   }
+    logging.config.dictConfig(LOGGING_CONFIG)
+
+
+class Settings(BaseSettings):
+    BOT_TOKEN: str
+    #ADMIN_ID: int
+    #DB_URL: str
+    model_config = SettingsConfigDict(env_file='.env')
+
+settings = Settings()
--- main.py
+++ main.py
@@ -2,30 +2,40 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 import logging
 
+from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 
-from config import BOT_TOKEN, ADMIN_ID, setup_logging
+from config import settings, setup_logging
 
 setup_logging()
 logger = logging.getLogger(__name__)
 
 
-bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-dp = Dispatcher()
+async def on_startup(bot) -> None:
+    logger.info("Бот запущен и готов к работе!")
 
-async def on_shutdown(bot):
-    logger.info("Бот упал")
 
-async def main():
-    try:
-        logger.info("Бот запущен!")
+async def on_shutdown(bot) -> None:
+    logger.info("Бот остановлен")
 
-        dp.shutdown.register(on_shutdown)
-        await bot.delete_webhook(drop_pending_updates=True)
-        await dp.start_polling(bot)
-    except Exception as e:
-        logger.critical(f"Бот не смог запустится! Критическая ошибка:"
-                        f"{e}")
+async def main() -> None:
+
+    bot = Bot(
+        token=settings.BOT_TOKEN,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+    dp = Dispatcher()
+
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    logger.info("Бот в процессе запуска...")
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except Exception as e:
+        logger.critical(f"Совершена критическая ошибка при запуске. Ошибка: {e}")
